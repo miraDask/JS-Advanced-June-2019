e42b9e3e79cea080129c77fdccdd527f998c9a32
miraDask <mira.daskalov@gmail.com>
2019-06-18 22:03:10 +0300

    refactoring and typo

--- 02.DOM MANIPULATIONS/Exercise/09. Distance-Converter/distanceConverter.js
+++ 02.DOM MANIPULATIONS/Exercise/09. Distance-Converter/distanceConverter.js
@@ -8,7 +8,7 @@ function attachEventsListeners() {
 		yrd: 0.9144,
 		ft: 0.3048,
 		in: 0.0254
-	}
+	};
 
 	const SELECTORS = {
 		CONVERT_BTN: 'convert',
@@ -16,24 +16,24 @@ function attachEventsListeners() {
 		OUTPUT_FIELD: 'outputUnits',
 		INPUT_DISTANCE: 'inputDistance',
 		OUTPUT_DISTANCE: 'outputDistance'
-	}
+	};
 
 	const convertBtn = document.getElementById(SELECTORS.CONVERT_BTN);
 	const inputUnitsElement = document.getElementById(SELECTORS.INPUT_FIELD);
 	const outputUnitsElement = document.getElementById(SELECTORS.OUTPUT_FIELD);
 
-	const convert = (distance, convertFrom, convertTo) => {
+	const convertDistance = (distance, convertFrom, convertTo) => {
 		const distanceToMeters = DISTANCE_TO_METERS[convertFrom] * distance;
 		return distanceToMeters / DISTANCE_TO_METERS[convertTo];
-	}
+	};
 
-	const handleConvertion = () => {
+	const handleConversion = () => {
 		const inputDistance = document.getElementById(SELECTORS.INPUT_DISTANCE).value;
 		const convertFrom = inputUnitsElement.options[inputUnitsElement.selectedIndex].value;
 		const convertTo = outputUnitsElement.options[outputUnitsElement.selectedIndex].value;
-		const convertedDistance = convert(inputDistance, convertFrom, convertTo);
+		const convertedDistance = convertDistance(inputDistance, convertFrom, convertTo);
 		document.getElementById(SELECTORS.OUTPUT_DISTANCE).value = convertedDistance;
-	}
+	};
 
-	convertBtn.addEventListener('click', handleConvertion)
+	convertBtn.addEventListener('click', handleConversion)
 }
