6c4a8379a5321a0a657c0d6a20a5dcb453a7b65a
miraDask <mira.daskalov@gmail.com>
2019-06-19 12:24:26 +0300

    initial commit

---
+++ 03.UNIT TESTING/Exercise/01.sub_sum/app.js
@@ -0,0 +1,18 @@
+function sumRangeOfNumbers(arr, startIndex, endIndex) {
+    const isNotArrayOfNumbers = (arr) =>{
+      return  arr.some(x => x !== +x);
+    }
+    
+    if (!Array.isArray(arr) || isNotArrayOfNumbers(arr)) {
+        return NaN;
+    } else if (startIndex < 0 || arr.length <= startIndex)  {
+        startIndex = 0;
+    } else if (endIndex >= arr.length) {    // if is < 0 check ?
+        endIndex = arr.length - 1;
+    }
+
+    const subArr = arr.slice(startIndex, endIndex + 1);
+    return subArr.reduce((a, b) => a + b, 0);
+}
+
+//console.log(sumRangeOfNumbers(3, 0, 2));
