68c4c46fd3a83f3ff8053f8e232fbacde309189b
miraDask <mira.daskalov@gmail.com>
2019-06-21 14:50:58 +0300

    adding solution for problem 3

---
+++ 03.UNIT TESTING/Lab/04.sum_of_numbers/sum.test.js
@@ -0,0 +1,35 @@
+const sum = require('../04.sum_of_numbers/sum').sum;
+const expect = require('chai').expect;
+
+describe('sum', function () {
+    it('expects to return correct result when array of numbers is passed', function () {
+        const result = sum([1, 2, 3]);
+        expect(result).to.be.equal(6);
+    })
+
+    it('expects to return 0 if empty array is passed', function () {
+        const result = sum([]);
+        expect(result).to.be.equal(0);
+    })
+
+    it('expects to throw an error if argument is not an array', function () {
+        const resultWithNumbers = function () {
+            sum(1, 2, 3)
+        };
+        const resultWithObject = function () {
+            sum({
+                a: 1
+            })
+        };
+
+        expect(resultWithNumbers).to.throw('arr is not iterable');
+        expect(resultWithObject).to.throw('arr is not iterable');
+
+    })
+
+    it('expects result to be NaN if an array of strings that cannot be parsed to numbers is passed', function () {
+        const result = sum(['a', 'b', 'c']);
+
+        expect(result).to.be.NaN;
+    })
+})
