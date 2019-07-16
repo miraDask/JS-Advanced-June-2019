bffe42513a4251193e16e29225f44c91122eefca
miraDask <mira.daskalov@gmail.com>
2019-07-16 17:02:07 +0300

    refactoring - 100points in judge System

--- Exam/01. SoftDo/scripts/index.js
+++ Exam/01. SoftDo/scripts/index.js
@@ -31,12 +31,12 @@ function mySolution() {
         const span = createElement('span', '', username);
         const p = createElement('p', '', text);
 
-        const divBtnHolder = createElement('div', 'actions');
+        const divBtnWrapper = createElement('div', 'actions');
         const archiveBtn = createElement('button', 'archive', 'Archive');
         const openBtn = createElement('button', 'open', 'Open');
-        
-        parentAppendChildren([archiveBtn, openBtn], divBtnHolder);
-        parentAppendChildren([img, span, p , divBtnHolder], divWrapper);
+
+        parentAppendChildren([archiveBtn, openBtn], divBtnWrapper);
+        parentAppendChildren([img, span, p, divBtnWrapper], divWrapper);
         elements.pendingField.appendChild(divWrapper);
 
         archiveBtn.addEventListener('click', function () {
@@ -47,75 +47,75 @@ function mySolution() {
 
         elements.textArea.value = '';
         elements.usernameArea.value = '';
-    
-        function loadQuestionInOpenQuestionsSection() {
-            const divWrapper = createElement('div', 'openQuestion');
-            const img = createImgElement(imgSrc, 32, 32);
-            const span = createElement('span', '', username);
-            const p = createElement('p', '', text);
-    
-            const divBtnWrapper = createElement('div', 'actions');
-            const replyBtn = createElement('button','reply', 'Reply');
-            replyBtn.addEventListener('click', replyOnCurrentQuestion);
-            parentAppendChildren([replyBtn], divBtnWrapper);
-            parentAppendChildren([img, span, p, divBtnWrapper], divWrapper);
+    }
 
-            const divReplySection = createElement('div', 'replySection');
+    function loadQuestionInOpenQuestionsSection() {
+        const divWrapper = this.parentNode.parentNode;
+        divWrapper.className = 'openQuestion';
+
+        const divBtnWrapper = divWrapper.lastChild;
+        divBtnWrapper.removeChild(divBtnWrapper.firstChild);
+        divBtnWrapper.removeChild(divBtnWrapper.firstChild);
+
+        const replyBtn = createElement('button', 'reply', 'Reply');
+        replyBtn.addEventListener('click', displayReplySection);
+        parentAppendChildren([replyBtn], divBtnWrapper);
+
+        const divReplySection = createElement('div', 'replySection');
+        divReplySection.style.display = 'none';
+        const input = createElement('input', 'replyInput');
+        input.type = 'text';
+        input.placeholder = 'Reply to this question here...';
+        const sendReplyButton = createElement('button', 'replyButton', 'Send');
+        sendReplyButton.addEventListener('click', sendReply);
+        const ol = createElement('ol', 'reply');
+        ol.type = '1';
+
+        parentAppendChildren([input, sendReplyButton, ol], divReplySection);
+        parentAppendChildren([divReplySection], divWrapper);
+
+        elements.pendingField.removeChild(divWrapper);
+        elements.openQuestionsField.appendChild(divWrapper);
+    }
+
+    function displayReplySection() {
+        const divReplySection = this.parentNode.parentNode.lastChild;
+
+        if (this.textContent === 'Reply') {
+            this.textContent = 'Back';
+            divReplySection.style.display = 'block';
+        } else {
+            this.textContent = 'Reply';
             divReplySection.style.display = 'none';
-            const input = createElement('input','replyInput');
-            input.type = 'text';
-            input.placeholder = 'Reply to this question here...';
-            const sendReplyButton = createElement('button', 'replyButton','Send');
-            const ol = createElement('ol', 'reply');
-            ol.setAttribute('type', '1');
+        }
+    }
 
-            parentAppendChildren([input,sendReplyButton, ol], divReplySection);
-            parentAppendChildren([divReplySection], divWrapper);
-    
-            elements.openQuestionsField.appendChild(divWrapper);
-    
-            this.parentNode.parentNode.remove();
+    function sendReply() {
+        const divReplySection = this.parentNode;
+        const input = divReplySection.firstChild;
+        const answer = input.value;
+        const orderedList = divReplySection.lastChild;
 
-            function replyOnCurrentQuestion() {
-                if (this.textContent === 'Reply') {
-                    this.textContent = 'Back';
-                    divReplySection.style.display = 'block';
-
-                    sendReplyButton.addEventListener('click', () => {
-                        const answer = input.value;
-        
-                        if (answer) {
-                            const li = createElement('li', '', answer);
-                            ol.appendChild(li);
-                            input.value = '';
-                        }
-                    });
-                } else {
-                    this.textContent = 'Reply';
-                    divReplySection.style.display = 'none';
-                    input.value = '';
-                }
-            }
-
-            // function createDivWrapper(divWrapperClassName, ) {
-
-            // }
+        if (answer) {
+            const listItem = createElement('li', '', answer);
+            parentAppendChildren([listItem], orderedList);
+            input.value = '';
         }
     }
 
     function createElement(tagName, className, text) {
         const element = document.createElement(tagName);
 
-        if(className) {
+        if (className) {
             element.className = className;
         }
 
-        if(text) {
+        if (text) {
             element.textContent = text;
         }
 
         return element;
-    } 
+    }
 
     function createImgElement(src, width, height) {
         const img = document.createElement('img');
@@ -131,4 +131,3 @@ function mySolution() {
         });
     }
 }
-// To check out your solution, just submit mySolution() function in judge system.
